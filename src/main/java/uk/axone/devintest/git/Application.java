af3cceb37d1f5e651ca71f6618a65ad35ffb5254
nagasarada <lillymaster10@gmail.com>
2023-05-31 15:29:42 +0100

    added test2

--- src/main/java/uk/axone/devintest/git/Application.java
+++ src/main/java/uk/axone/devintest/git/Application.java
@@ -3,5 +3,6 @@ package uk.axone.devintest.git;
 public class Application {
     public static void main(String[] args) {
         System.out.println("git is cool");
+        System.out.println("added another test today");
     }
 }
